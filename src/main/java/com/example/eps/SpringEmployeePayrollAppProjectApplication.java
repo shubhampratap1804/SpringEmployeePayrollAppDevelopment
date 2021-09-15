1c9acb5e5145106c16af6bc55711cec83af4c203
shubhampratap1804 <samsunggalaxygrand2pratap@gmail.com>
2021-09-15 22:43:57 +0530

    Added: welcome message

---
+++ src/main/java/com/example/eps/SpringEmployeePayrollAppProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.eps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeePayrollAppProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee payroll app development!");
+		SpringApplication.run(SpringEmployeePayrollAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eps/SpringEmployeePayrollAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeePayrollAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
